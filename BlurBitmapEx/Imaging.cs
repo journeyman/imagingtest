76e1ddc18ab01de3758e79f6054c46f43c1005bf
journeyman <journeymanmw@gmail.com>
2015-01-08 21:04:28 +0000

    draft Nokia implementation, setup BitmapEx project

---
+++ BlurBitmapEx/Imaging.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace BlurBitmapEx
+{
+    public class Imaging
+    {
+        public async Task<WriteableBitmap> Blur(Stream stream, int kernelSize)
+        {
+            
+        }
+    }
+}
---
+++ BlurNokia/Imaging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Lumia.Imaging;
+using Lumia.Imaging.Adjustments;
+
+namespace BlurNokia
+{
+    public class Imaging
+    {
+        public async Task<WriteableBitmap> Blur(Stream stream, int kernelSize)
+        {
+            using (var source = new StreamImageSource(stream))
+            {
+                using (var filters = new FilterEffect(source))
+                {
+                    var blur = new BlurFilter(kernelSize);
+                    filters.Filters = new[] {blur};
+
+                    //var output = new WriteableBitmap(pixelWidth, pixelHeight);
+                    using (var renderer = new WriteableBitmapRenderer(filters))
+                    {
+                        return await renderer.RenderAsync();
+                    }
+                }
+            }
+
+        }
+    }
+}
