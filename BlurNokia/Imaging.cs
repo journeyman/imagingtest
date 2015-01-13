cc83838dea6da0ba517dc0218e69ef77c4ba3f2b
stas.shusha <stas.shusha@corp.badoo.com>
2015-01-13 15:11:25 +0000

    added alternative Blur2 implementation of Nokia blur (resize first, then blur)

--- BlurNokia/Imaging.cs
+++ BlurNokia/Imaging.cs
@@ -14,29 +14,54 @@ namespace BlurNokia
         public static async Task<WriteableBitmap> Blur(Stream stream, int kernelSize, Size size)
         {
             using (var source = new StreamImageSource(stream))
+            using (var filters = new FilterEffect(source))
             {
-                using (var filters = new FilterEffect(source))
+                var blur = new BlurFilter(kernelSize);
+
+                filters.Filters = new[] { blur };
+
+                using (var renderer = new BitmapRenderer(filters))
+                {
+                    renderer.Size = size;
+                    renderer.OutputOption = OutputOption.PreserveAspectRatio;
+                        
+                    var bitmap = await renderer.RenderAsync();
+                    var target = new WriteableBitmap((int)bitmap.Dimensions.Width, (int)bitmap.Dimensions.Height);
+
+                    using (var bitmapSource = new BitmapImageSource(bitmap))
+                    using (var rend = new WriteableBitmapRenderer(bitmapSource, target))
+                    {
+                        return await rend.RenderAsync().AsTask().ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        public static async Task<WriteableBitmap> Blur2(Stream stream, int kernelSize, Size size)
+        {
+            using (var source = new StreamImageSource(stream))
+            using (var renderer = new BitmapRenderer(source))
+            {
+                renderer.Size = size;
+                renderer.OutputOption = OutputOption.PreserveAspectRatio;
+
+                var bitmap = await renderer.RenderAsync();
+
+                using (var bitmapSource = new BitmapImageSource(bitmap))
+                using (var filters = new FilterEffect(bitmapSource))
                 {
                     var blur = new BlurFilter(kernelSize);
 
                     filters.Filters = new[] { blur };
 
-                    using (var renderer = new BitmapRenderer(filters))
-                    {
-                        renderer.Size = size;
-                        renderer.OutputOption = OutputOption.PreserveAspectRatio;
-                        
-                        var bitmap = await renderer.RenderAsync();
-                        var target = new WriteableBitmap((int)bitmap.Dimensions.Width, (int)bitmap.Dimensions.Height);
+                    var target = new WriteableBitmap((int)bitmap.Dimensions.Width, (int)bitmap.Dimensions.Height);
 
-                        using (var rend = new WriteableBitmapRenderer(new BitmapImageSource(bitmap), target))
-                        {
-                            return await rend.RenderAsync().AsTask().ConfigureAwait(false);
-                        }
+                    using (var rend = new WriteableBitmapRenderer(filters, target))
+                    {
+                        return await rend.RenderAsync().AsTask().ConfigureAwait(false);
                     }
                 }
             }
-
         }
 
     }
